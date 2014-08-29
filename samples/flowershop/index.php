d335b687df53492360a03220b77900baab979da8
sebbekarlsson <sebastiankarlsson@sebastians-air.lan>
2014-08-29 18:13:58 +0200

    Update

---
+++ samples/flowershop/index.php
@@ -0,0 +1,43 @@
+<html>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link href='http://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
+	
+<header>
+	<div class="header" align="center">
+		Henry's Flowers
+	</div>
+	<div class="header" align="center">
+		<img src="images/flowers.jpg" width="100%"><br>
+		Home
+	</div>
+	
+	<div align="center" class="headermenu">
+		<a href="#" class="menubutton">Home</a>
+		<a href="shop.php" class="menubutton">Shop</a>
+		<a href="#" class="menubutton">Contact</a>
+	</div>
+</header>
+	
+	
+<body>
+<div align="center">
+	<p><img src="images/henry.png"><br>
+	<font face="Reenie Beanie" size="16">
+		
+		My name is Henry.<br>
+		Welcome to my Flower shop!
+		
+	</font>
+</div>	
+	
+</body>
+	
+	
+<footer>
+	
+	
+</footer>
+	
+</link>
+</link>
+</html>
---
+++ samples/flowershop/shop.php
@@ -0,0 +1,59 @@
+<html>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link href='http://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
+	
+<header>
+	<div class="header" align="center">
+		Henry's Flowers
+	</div>
+	<div class="header" align="center">
+		<img src="images/flowers.jpg" width="100%"><br>
+		Shop
+	</div>
+	
+	<div align="center" class="headermenu">
+		<a href="index.php" class="menubutton">Home</a>
+		<a href="#" class="menubutton">Shop</a>
+		<a href="#" class="menubutton">Contact</a>
+	</div>
+</header>
+	
+	
+<body>
+
+<div align="center">
+<font face="Reenie Beanie" size="17">
+	<p>Here's the pretty flowers i'm selling.
+	<hr>
+</font>
+</div>
+
+<div class="itembox" id="itemboxleft">
+	<center>
+	<h1>Tulip</h1>
+	<p><img src="images/tulip.jpg" width="124px" height="124px">
+	<p><a href="#" class="menubutton">Buy</a>
+	</center>
+</div>
+
+
+<div class="itembox" id="itemboxcenter">
+	<center>
+	<h1>SunFlower</h1>
+	<p><img src="images/sunflower.jpeg" width="124px" height="124px">
+	<p><a href="#" class="menubutton">Buy</a>
+	</center>
+</div>
+	
+	
+</body>
+	
+	
+<footer>
+	
+	
+</footer>
+	
+</link>
+</link>
+</html>
